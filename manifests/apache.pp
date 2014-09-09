ba299a34ac3e6200dce9415ec3fbdc6fa6640457
Russell Sim <russell.sim@gmail.com>
2014-09-09 01:34:35 +0000

    Added .conf to end of apache config files

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -2,7 +2,7 @@ class graphite::apache (
   $service = 'apache2'
 ) {
 
-  file { '/etc/apache2/sites-available/graphite':
+  file { '/etc/apache2/sites-available/graphite.conf':
     ensure  => present,
     source  => "puppet:///modules/${module_name}/etc/graphite-web/apache.conf",
     owner   => 'root',
@@ -12,14 +12,14 @@ class graphite::apache (
     notify  => Service[$service],
   }
 
-  file { '/etc/apache2/sites-enabled/graphite':
+  file { '/etc/apache2/sites-enabled/graphite.conf':
     ensure  => link,
-    target  => '/etc/apache2/sites-available/graphite',
+    target  => '/etc/apache2/sites-available/graphite.conf',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     require => [Package[$graphite::params::package_web],
-                File['/etc/apache2/sites-available/graphite']],
+                File['/etc/apache2/sites-available/graphite.conf']],
     notify  => Service[$service],
   }
 
